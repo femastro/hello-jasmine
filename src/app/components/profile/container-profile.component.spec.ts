39df57f70a4738ed73a3e0e8217141096412e61f
Fernando <38165979+femastro@users.noreply.github.com>
2021-07-24 15:22:23 -0300

    Taller test 24/7

--- src/app/components/profile/container-profile.component.spec.ts
+++ src/app/components/profile/container-profile.component.spec.ts
@@ -1,16 +1,27 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { ContainerProfileComponent } from './container-profile.component';
+import { HttpClientTestingModule } from '@angular/common/http/testing'
+import { NO_ERRORS_SCHEMA } from '@angular/compiler';
+import { DataService } from 'src/app/data.service';
+import { of } from 'rxjs';
 
 describe('ContainerProfileComponent', () => {
   let component: ContainerProfileComponent;
   let fixture: ComponentFixture<ContainerProfileComponent>;
 
   beforeEach(async () => {
+    const getUsersSpy = jasmine.createSpyObj<DataService>(['getAllUsers']);
+    getUsersSpy.getAllUsers.and.returnValues(of([]))
+
     await TestBed.configureTestingModule({
-      declarations: [ ContainerProfileComponent ]
+      declarations: [ContainerProfileComponent],
+      imports: [HttpClientTestingModule],
+      schemas: [NO_ERRORS_SCHEMA],
+      providers: [
+        { provide: DataService, userValue: getUsersSpy }
+      ],
     })
-    .compileComponents();
+      .compileComponents();
   });
 
   beforeEach(() => {
